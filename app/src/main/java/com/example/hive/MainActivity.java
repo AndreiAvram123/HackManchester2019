a635d1e92ff0b6232c0ec24bfdfb132b3734c951
avram <avramandreitiberiu@gmail.com>
2019-10-26 22:17:39 +0100

    -map feature added

--- app/src/main/java/com/example/hive/MainActivity.java
+++ app/src/main/java/com/example/hive/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.hive;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.hive.adapters.MainRecyclerAdapter;
@@ -17,7 +21,8 @@ public class MainActivity extends AppCompatActivity implements TeachFragment.Tea
 
     private FragmentManager fragmentManager;
     private MainFragment mainFragment;
-
+    private static final int CODE_FINE_LOCATION = 2;
+    private static final int CODE_COURSE_LOCATION = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +33,29 @@ public class MainActivity extends AppCompatActivity implements TeachFragment.Tea
                 .replace(R.id.container_main,mainFragment)
                 .addToBackStack(null)
                 .commit();
+
+
+        getPermissions();
+    }
+
+    private void getPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //request fine location permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    CODE_FINE_LOCATION);
+
+        }else{
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                //request fine location permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        CODE_COURSE_LOCATION);
+
+            }
+        }
     }
 
 
--- app/src/main/java/com/example/hive/fragments/MapsFragment.java
+++ app/src/main/java/com/example/hive/fragments/MapsFragment.java
@@ -1,23 +1,22 @@
 package com.example.hive.fragments;
 
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.hive.R;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,8 +25,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     MapView mapView;
     GoogleMap map;
-    private static final int CODE_FINE_LOCATION = 2;
-    private static final int CODE_COURSE_LOCATION = 3;
+
 
     public static MapsFragment newInstance(){
         return new MapsFragment();
@@ -36,32 +34,12 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_maps, container, false);
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            //request fine location permission
-            ActivityCompat.requestPermissions(getActivity(),
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    CODE_FINE_LOCATION);
 
-        }else{
-             if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED) {
-                //request fine location permission
-                ActivityCompat.requestPermissions(getActivity(),
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        CODE_COURSE_LOCATION);
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) v.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
 
-            }else{
-                 // Gets the MapView from the XML layout and creates it
-                 mapView = (MapView) v.findViewById(R.id.mapView);
-                 mapView.onCreate(savedInstanceState);
-
-
-                 mapView.getMapAsync(this);
-
-             }
-        }
-
+        mapView.getMapAsync(this);
 
 
         return v;
@@ -69,11 +47,15 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d("Test","Map ready");
         map = googleMap;
-        map.getUiSettings().setMyLocationButtonEnabled(false);
-        map.setMyLocationEnabled(true);
+        googleMap.setMyLocationEnabled(true);
         LatLng latLng = new LatLng(53.47723, -2.25487);
+        map.addMarker(new MarkerOptions().position(latLng).title("Marker in Industry museum"));
+        CameraUpdate location = CameraUpdateFactory.newLatLngZoom(
+                latLng,15);
         map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        map.animateCamera(location);
 
     }
 
